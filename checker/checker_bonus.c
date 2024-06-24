4b068c4ecf6e9072d94501330ee0a92229db0c27
Jinkwon Seo <jinseo@c3r8s3.42gyeongsan.kr>
2024-06-25 02:56:56 +0900

    check norm in checker

--- checker/checker_bonus.c
+++ checker/checker_bonus.c
@@ -1,57 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinseo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 02:53:26 by jinseo            #+#    #+#             */
+/*   Updated: 2024/06/25 02:55:19 by jinseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/checker_bonus.h"
 
-void is_error(t_stack *a, t_stack *b, char *cmd, char *str)
+void	is_error(t_stack *a, t_stack *b, char *cmd, char *str)
 {
-    ft_putendl_fd(str, 1);
+	ft_putendl_fd(str, 1);
 	ft_free_stack(a);
 	ft_free_stack(b);
-    free(cmd);
+	free(cmd);
 	exit(1);
 }
 
-int is_cmd(t_stack *a, t_stack *b, char *cmd)
+int	is_cmd(t_stack *a, t_stack *b, char *cmd)
 {
-    if (!ft_memcmp("sa\n", cmd, 3))
-        return(sa_b(a));
-    else if (!ft_memcmp("sb\n", cmd, 3))
-        return(sb_b(b));
-    else if (!ft_memcmp("ss\n", cmd, 3))
-        return(ss_b(a, b));
-    else if (!ft_memcmp("pa\n", cmd, 3))
-        return(pa_b(a, b));
-    else if (!ft_memcmp("pb\n", cmd, 3))
-        return(pb_b(a, b));
-    else if (!ft_memcmp("ra\n", cmd, 3))
-        return(ra_b(a));
-    else if (!ft_memcmp("rb\n", cmd, 3))
-        return(rb_b(b));
-    else if (!ft_memcmp("rr\n", cmd, 3))
-        return(rr_b(a, b));
-    else if (!ft_memcmp("rra\n", cmd, 4))
-        return(rra_b(a));
-    else if (!ft_memcmp("rrb\n", cmd, 4))
-        return(rrb_b(b));
-    else if (!ft_memcmp("rrr\n", cmd, 4))
-        return(rrr_b(a, b));
-    else
-        is_error(a, b, cmd, "KO");
-    return(1);
+	if (!ft_memcmp("sa\n", cmd, 3))
+		return (sa_b(a));
+	else if (!ft_memcmp("sb\n", cmd, 3))
+		return (sb_b(b));
+	else if (!ft_memcmp("ss\n", cmd, 3))
+		return (ss_b(a, b));
+	else if (!ft_memcmp("pa\n", cmd, 3))
+		return (pa_b(a, b));
+	else if (!ft_memcmp("pb\n", cmd, 3))
+		return (pb_b(a, b));
+	else if (!ft_memcmp("ra\n", cmd, 3))
+		return (ra_b(a));
+	else if (!ft_memcmp("rb\n", cmd, 3))
+		return (rb_b(b));
+	else if (!ft_memcmp("rr\n", cmd, 3))
+		return (rr_b(a, b));
+	else if (!ft_memcmp("rra\n", cmd, 4))
+		return (rra_b(a));
+	else if (!ft_memcmp("rrb\n", cmd, 4))
+		return (rrb_b(b));
+	else if (!ft_memcmp("rrr\n", cmd, 4))
+		return (rrr_b(a, b));
+	else
+		is_error(a, b, cmd, "KO");
+	return (1);
 }
 
-void input_cmd(t_stack *a, t_stack *b)
+void	input_cmd(t_stack *a, t_stack *b)
 {
-    char *cmd;
+	char	*cmd;
 
-    cmd = get_next_line(0);
-    while (cmd)
-    {
-        if(is_cmd(a, b, cmd) == -1)
-            is_error(a, b, cmd, "KO");
-        if (cmd)
-            free(cmd);
-        cmd = get_next_line(0);
-    }
-    free(cmd);
+	cmd = get_next_line(0);
+	while (cmd)
+	{
+		if (is_cmd(a, b, cmd) == -1)
+			is_error(a, b, cmd, "KO");
+		if (cmd)
+			free(cmd);
+		cmd = get_next_line(0);
+	}
+	free(cmd);
 }
 
 int	main(int argc, char **argv)
@@ -62,12 +74,12 @@ int	main(int argc, char **argv)
 	a = ft_creat_stack();
 	b = ft_creat_stack();
 	ft_judge(a, b, argv, argc);
-    input_cmd(a, b);
-    if(!ft_is_sort2(a) && b->top == NULL)
-        is_error(a, b, NULL, "OK");
-    else
-    {
-        is_error(a, b, NULL, "KO");
-    }
+	input_cmd(a, b);
+	if (!ft_is_sort2(a) && b->top == NULL)
+		is_error(a, b, NULL, "OK");
+	else
+	{
+		is_error(a, b, NULL, "KO");
+	}
 	return (0);
 }
